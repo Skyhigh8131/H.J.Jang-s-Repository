e336bb2570013afac30464033a9fa9f003fa761a
Skyhigh8131 <62192876+Skyhigh8131@users.noreply.github.com>
2020-04-09 13:56:02 +0900

    Update DinosaurManager.java

--- src/DinosaurManager.java
+++ src/DinosaurManager.java
@@ -3,12 +3,59 @@ import java.util.Scanner;
 public class DinosaurManager {
 	
 	Scanner input;
-	
 	DinosaurManager(Scanner input){
 		this.input = input;
 	}
-	
-
+	public static void information() {
+		System.out.println("Select your dinosaur");
+		String[] time = {"Triassic", "Jurassic", "Cretaceous"};
+		String[] preference = {"Carnivor!", "Herbivorous!"};
+		String[] size = {"Big one", "Small one"};
+		
+		Scanner input = new Scanner(System.in);
+		String dinosaur = input.nextLine();
+		
+		switch(dinosaur) {
+		case "T Rex":
+			System.out.println(time[2]);
+			System.out.println(preference[0]);
+			System.out.println(size[0]);
+			break;
+		case "Spinosaurus":
+			System.out.println(time[2]);
+			System.out.println(preference[0]);
+			System.out.println(size[0]);
+			break;
+		case "Stegosaurus":
+			System.out.println(time[1]);
+			System.out.println(preference[1]);
+			System.out.println(size[0]);
+			break;
+		case "Velociraptor":
+			System.out.println(time[2]);
+			System.out.println(preference[0]);
+			System.out.println(size[1]);
+			break;
+		case "Triceratops":
+			System.out.println(time[2]);
+			System.out.println(preference[1]);
+			System.out.println(size[0]);
+			break;
+		case "Brachiosaurus":
+			System.out.println(time[1]);
+			System.out.println(preference[1]);
+			System.out.println(size[0]);
+			break;
+		case "Driosaurus":
+			System.out.println(time[1]);
+			System.out.println(preference[1]);
+			System.out.println(size[1]);
+			break;
+		default:
+			System.out.printf("%s is not in this park.\n", dinosaur);
+		}
+		
+	}
 	public static void feedDinosaur() {
 		System.out.println("Select your dinosaur");
 		
@@ -17,9 +64,32 @@ public class DinosaurManager {
 		
 		System.out.printf("Your dinosaur is %s! \n", dinosaur);
 		
-		System.out.printf("Choose the meal of dinosaur. ");
-		String meal = input.nextLine();
-		System.out.printf("Its meal is %s \n", meal);
+		String[] preference = {"Carnivor!", "Herbivorous !"};
+		String[] meal = {"Meat", "Haycock"};
+		switch(dinosaur) {
+		case ("T Rex"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[0], meal[0], dinosaur);
+		    break;
+		case ("Spinosaurus"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[0], meal[0], dinosaur);
+		    break;
+		case ("Stegosaurus"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[1], meal[1], dinosaur);
+		    break;
+		case ("Velociraptor"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[0], meal[0], dinosaur);
+		    break;
+		case ("Triceratops"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[1], meal[1], dinosaur);
+	        break;
+		case ("Brachiosaurus"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[1], meal[1], dinosaur);
+            break;
+		case ("Driosaurus"):
+			System.out.printf("%s is %s, so %s will be given to %s. \n", dinosaur, preference[1], meal[1], dinosaur);
+            break;
+		}
+		
 	}
 	public static void fightDinosaur() {
 		System.out.println("Select a dinosaur.");
@@ -38,15 +108,38 @@ public class DinosaurManager {
 		System.out.println("Select a dinosaur.");
 		Scanner input = new Scanner (System.in);
 		String Dinosaur = input.nextLine();
-		
 		System.out.printf("%s has killed by Jurassic Park's guard, safelly. \n", Dinosaur);
 	}
 	public static void Makesleep() {
 		System.out.println("Select a dinosaur.");
 		Scanner input = new Scanner (System.in);
 		
+		String[] size = {"Big one", "Small one"};
 	    String dinosaur = input.nextLine();
-	    System.out.printf("%s will fall asleep. \n", dinosaur);    
+	    
+	    switch(dinosaur) {
+	    case ("T Rex"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[0]);
+	        break;
+	    case ("Spinosaurus"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[0]);
+            break;
+	    case ("Stegosaurus"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[0]);
+            break;
+	    case ("Velociraptor"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[1]);
+	        break;
+	    case ("Triceratops"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[0]);
+	    case ("Brachiosaurus"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[0]);
+	        break;
+	    case ("Driosaurus"):
+	    	System.out.printf("%s is %s.\n", dinosaur, size[1]);
+	        break;
+	    }
+	    System.out.printf("%s will fall asleep. \n", dinosaur);
 	}
 	public static void Raisedinosaur() {
 		System.out.println("Select a dinosaur.");
