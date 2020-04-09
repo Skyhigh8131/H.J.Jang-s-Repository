da9775b76c25bd37c4cde7301330855eb05c9308
Skyhigh8131 <62192876+Skyhigh8131@users.noreply.github.com>
2020-04-09 14:02:44 +0900

    Update MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -12,37 +12,37 @@ public class MenuManager {
 		
 		Scanner input = new Scanner(System.in);
 		DinosaurManager dm = new DinosaurManager(input);
-		int num = 5;
-		while (num != 6) {
-			System.out.println("Select one number between 1 ~ 6 ");
-			System.out.println("0. information of your dinosaur");
-			System.out.printf("1. Feed to dinosaur. \n");
-			System.out.printf("2. Make quarrel with anotehr dinasours. \n");
-			System.out.printf("3. Administer euthanasia dinosaur \n");
-			System.out.printf("4. Make sleep dinosaur \n");
-			System.out.printf("5. Raise dinosaurs. \n");
-			System.out.printf("6. Exit. \n");
+		int num = 7;
+		while (num != 8) {
+			System.out.println("Select one number between 1 ~ 7.\n");
+			System.out.println("1. Information of your dinosaur\n");
+			System.out.println("2. Feed to dinosaur. \n");
+			System.out.println("3. Make quarrel with anotehr dinasours. \n");
+			System.out.println("4. Administer euthanasia dinosaur \n");
+			System.out.println("5. Make sleep dinosaur \n");
+			System.out.println("6. Raise dinosaurs. \n");
+			System.out.println("7. Exit. \n");
 			num = input.nextInt();
-			if (num == 0) {
+			if (num == 1) {
 				dm.information();
 			}
-		    if (num == 1) {
+			else if (num == 2) {
 				dm.feedDinosaur();
 			}
-			else if (num == 2) {
+			else if (num == 3) {
 				dm.fightDinosaur();
 			}
-			else if (num == 3) {
+			else if (num == 4) {
 				dm.killDinosaur();
 			}
-			else if (num == 4) {
+			else if (num == 5) {
 				dm.Makesleep();
 			}
-			else if (num == 5) {
+			else if (num == 6) {
 				dm.Raisedinosaur();
 			}
-			else if (num == 6) {
-				System.out.println("Good bye");
+			else if (num == 7) {
+				System.out.println("Good bye.\n");
 			}
 		}
 	}
